05f604fbc1aec7eefdd9b4ee884a3495da5cff43
zehancoder <zehanjh93@gmail.com>
2025-02-02 02:06:45 +0600

    footer done

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,7 +1,54 @@
+import { ConItem } from "../common/Container";
+import { navItem } from "../../lib/db";
 export function FootFunc() {
-    return (
-        <>
-            footer
-        </>
-    )
+  return (
+    <footer className="pt-5 lg:pt-24">
+      <ConItem>
+        <div className="flex sm:flex-row flex-col px-3 items-center justify-between gap-2.5 py-5">
+          <h1 className="font-space xl:text-5xl lg:text-4xl sm:text-2xl text-xl cursor-pointer font-bold text-black">
+            Ai GoverningDocs
+          </h1>
+          <div className="hidden lg:flex items-center justify-center gap-3 xl:gap-4">
+            {navItem.map(({ label, link }) => (
+              <a
+                href={link}
+                className="text-accent transition-colors duration-150 text-sm hover:text-primary  xl:text-base"
+                key={label}
+              >
+                {label}
+              </a>
+            ))}
+          </div>
+          <div className="flex items-center justify-center gap-2.5">
+            {socialIcon.map(({img, url}, i) => (
+                <a href= {url} key={i}>
+                    <img src= {img} alt="" />
+                </a>
+            ))}
+          </div>
+        </div>
+        <div className="mt-[20px] border-t border-transparent md:mt-[50px] lg:mt-[106px]">
+            <p className="text-lg text-center leading-[18px] text-accent py-5">Ai GoverningDocs 2024. All Rights Reserved.</p>
+        </div>
+      </ConItem>
+    </footer>
+  );
 }
+const socialIcon = [
+    {
+        img: "/face.png",
+        url: "#"
+    },
+    {
+        img: "/ins.png",
+        url: "#"
+    },
+    {
+        img: "/link.png",
+        url: "#"
+    },
+    {
+        img: "/twi.png",
+        url: "#"
+    },
+]
